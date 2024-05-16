cc656db99d8592455015771c5862f71b12e37918
Victor Lindell <victor.lindell@youcruit.com>
2024-05-16 17:18:04 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# nodemailer-rest-transport
---
+++ nodemailer-rest-transport.ts
@@ -0,0 +1,59 @@
+import * as nodemailer from 'nodemailer'
+
+export class RestTransport implements nodemailer.Transport {
+    name: string
+    version: string
+    private host: string
+    private port: number
+    private api_key: string
+
+    constructor(args:
+        {host: string, port: number, auth: { api_key: string}}) {
+        this.host = args.host
+        this.port = args.port
+        this.api_key = args.auth.api_key
+        this.name = 'RestTransport'
+        this.version = '5'
+    }
+
+    send(mail: any, callback: (err: (Error | null), info: any) => void) {
+
+        const transporter = mail.mailer.transporter
+
+        if (!transporter || transporter.name !== 'RestTransport') {
+            callback(new Error('Invalid transporter'), null)
+            return
+        }
+
+        const { host, port } = transporter
+        const url = `http://${host}:${port}/tachikoma/sendEmail`
+
+        const payload = {
+            subject: mail.data.subject,
+            from: mail.data.from,
+            html: mail.data.html,
+            text: mail.data.text,
+            to: [{ address: mail.data.to }],
+            attachments: mail.data.attachments,
+            headers: mail.data.headers,
+        }
+
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(payload),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`)
+                }
+                callback(null, { ...response, messageId: response.id })
+
+            })
+            .then(data => {
+                callback(null, data)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
+    }
+}
